58b0eb39f868c5ddb9518e12f8da70f6ce29e6d9
Harish kumar <harishkumarp3303gmail.com>
2023-07-26 18:38:24 +0530

    gcdemo

--- Java/src/GarbageCollection/GCDemo.java
+++ Java/src/GarbageCollection/GCDemo.java
@@ -3,14 +3,38 @@ package GarbageCollection;
 public class GCDemo {
 
     public static void main(String[] args) {
-        
+
+        // Garbage collection is a process of automatic deletion of unused objects or unreferenced objects.
+
+        // 1.How objects can be unreferenced 
+
+        // a. By nulling the reference
+        GCDemo gcDemo=new GCDemo();
+        gcDemo=null; // nulling the reference and making eligible for garbage collection
+
+        // b. By assigning another reference to the object
+        GCDemo gcDemo1=new GCDemo();
+        GCDemo gcDemo2=new GCDemo();
+        gcDemo2=gcDemo1; // now geDemo1 is garbage collected
+
+        // c. By anonymous object
+        new GCDemo(); 
+
+
         GCDemo obj = new GCDemo();
         obj = null;
         System.gc();
         System.out.println("End of main");
     
     }
-    protected void finalize() throws Throwable { 
+
+    @Override
+    protected void finalize() throws Throwable { // Method Belongs to GC Demo class
         System.out.println("Object is garbage collected"); 
     };
     }
+
+// Output
+
+// End of main
+// Object is garbage collected
